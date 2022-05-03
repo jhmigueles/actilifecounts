dc381faa4eeb9836d82fb04da1fede0ee959e01f
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2022-05-03 11:58:31 +0200

    rename vars

--- R/bpf_filter.R
+++ R/bpf_filter.R
@@ -21,10 +21,10 @@ bpf_filter = function(downsample_data = c(), verbose = FALSE) {
   zi = t(matrix(rep(zi, 3), ncol = 3, byrow = F)) * downsample_data[1, ]
   zi = t(zi)
   # colnames(zi) = c("accX", "accY", "accZ")
-  bpf_data_x = gsignal::filter(filt = input_coeffs, a = output_coeffs, x = downsample_data[, 1], zi = zi[, 1])
-  bpf_data_y = gsignal::filter(filt = input_coeffs, a = output_coeffs, x = downsample_data[, 2], zi = zi[, 2])
-  bpf_data_z = gsignal::filter(filt = input_coeffs, a = output_coeffs, x = downsample_data[, 3], zi = zi[, 3])
-  bpf_data = as.matrix(cbind(bpf_data_x[[1]], bpf_data_y[[1]], bpf_data_z[[1]]))
+  x = gsignal::filter(filt = input_coeffs, a = output_coeffs, x = downsample_data[, 1], zi = zi[, 1])
+  y = gsignal::filter(filt = input_coeffs, a = output_coeffs, x = downsample_data[, 2], zi = zi[, 2])
+  z = gsignal::filter(filt = input_coeffs, a = output_coeffs, x = downsample_data[, 3], zi = zi[, 3])
+  bpf_data = as.matrix(cbind(x[[1]], y[[1]], z[[1]]))
   bpf_data = ((3.0 / 4096.0) / (2.6 / 256.0) * 237.5) * bpf_data
   if (verbose) cat(" Done!\n")
   return(bpf_data)
