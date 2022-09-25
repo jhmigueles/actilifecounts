9b158673cbb71a5a41039f643579f55d6c0ac51a
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2022-09-25 09:51:48 +0200

    Fixed test to account for new output column generated (vector magnitude counts)

--- tests/testthat/test-get_counts.R
+++ tests/testthat/test-get_counts.R
@@ -8,14 +8,14 @@ testthat::test_that("get_counts calculates counts as expected", {
   counts = get_counts(raw, sf = 100, epoch = 60, lfe_select = FALSE, verbose = TRUE)
 
   expect_equal(nrow(counts), 40)
-  expect_equal(ncol(counts), 3)
+  expect_equal(ncol(counts), 4)
   expect_equal(sum(counts[,1]), 57404)
 
   # get counts with lfe filter
   counts = get_counts(raw, sf = 100, epoch = 60, lfe_select = TRUE, verbose = FALSE)
 
   expect_equal(nrow(counts), 40)
-  expect_equal(ncol(counts), 3)
+  expect_equal(ncol(counts), 4)
   expect_equal(sum(counts[,1]), 59213)
 
 })
